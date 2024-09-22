d435abf5c30eec0a7bd72a2844469ca79052aac4
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2024-09-22 18:30:08 +0800

    update
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

--- bsp/sunxi/drivers/sun8i_r528_gpio.h
+++ bsp/sunxi/drivers/sun8i_r528_gpio.h
@@ -5,5 +5,6 @@
 #define GPIO_CFG(_grp, _x)    (0x30 * _grp + 0x4 * _x)
 
 #define GPIO_DAT(_x)    (0x30 * _x + 0x10)
+#define GPIO_PUPD(_x)   (0x30 * _x + 0x24)
 
 #endif
--- bsp/sunxi/drivers/sun8i_r528_pinctrl.c
+++ bsp/sunxi/drivers/sun8i_r528_pinctrl.c
@@ -10,17 +10,17 @@ void sunxi_pinctrl_init(const struct pinctrl *pinctrl)
     rt_uint32_t cfg_val;
     rt_uint32_t pull_val;
 
-    cfg_val = readl(GPIO_BASE + pinctrl->group * 0x30);
-    pull_val = readl(GPIO_BASE + pinctrl->group * 0x30 + 0x24);
+    cfg_val = readl(GPIO_BASE + GPIO_CFG(pinctrl->group, 0));
+    pull_val = readl(GPIO_BASE + GPIO_PUPD(pinctrl->group));
 
-    cfg_val &= ~(0xf << pinctrl->pin * 4);
-    cfg_val |= (pinctrl->func << pinctrl->pin * 4);
+    cfg_val &= ~(0xf << (pinctrl->pin * 4));
+    cfg_val |= (pinctrl->func << (pinctrl->pin * 4));
 
-    writel(cfg_val, GPIO_BASE + pinctrl->group * 0x30);
+    writel(cfg_val, GPIO_BASE + GPIO_CFG(pinctrl->group, 0));
 
     if (pinctrl->pull) {
-        pull_val &= ~(0x3 << pinctrl->pin * 2);
-        pull_val |= pinctrl->pull << pinctrl->pull * 2;
-        writel(pull_val, GPIO_BASE + pinctrl->group * 0x30 + 0x24);
+        pull_val &= ~(0x3 << (pinctrl->pin * 2));
+        pull_val |= pinctrl->pull << (pinctrl->pull * 2);
+        writel(pull_val, GPIO_BASE + GPIO_PUPD(pinctrl->group));
     }
 }
